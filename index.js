8b053edc1bd7ada6f7b6f3a8451a4d9766dba913
vtrcosta25 <viictoorrodrigues15@gmail.com>
2022-04-28 21:22:44 -0300

    check

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
